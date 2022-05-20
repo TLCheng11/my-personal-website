30a97b94fd8eb2f4bb0450b899510be8d7e63c10
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-05-20 18:16:51 -0400

    add and test effect for list clicking event

--- js/script.js
+++ js/script.js
@@ -6,3 +6,21 @@ myName.addEventListener("mouseover", function() {
        myName.style.color = ""; 
     }, 1000);
 });
+
+// to assign info on click behavior
+
+const info1 = document.getElementById("info_1");
+const detail1 = document.getElementById("detail_1");
+const content = document.getElementsByClassName("hide")
+info1.addEventListener("click", function() {
+    detail1.style.width = "100%";
+    detail1.style.opacity = "1";
+    setTimeout(function() {
+        for (let x = 0; x <= content.length - 1; x++) {
+            setTimeout(function() {
+                console.log(content[x])
+                content[x].style.opacity = "1";
+            }, 1000 * x);
+        }
+    }, 2000);
+});

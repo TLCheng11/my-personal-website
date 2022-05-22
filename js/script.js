e804092efa7e485a41ae0e4187a420de5078e258
Ting Luen Cheng <scorpio_ctl@hotmail.com>
2022-05-22 16:53:32 -0400

    make click behavior more smooth

--- js/script.js
+++ js/script.js
@@ -21,8 +21,9 @@ for (let i = 1; i <= divCount; i++) {
 
 function reset() {
     for (let i = 0; i < divCount; i++) {
-        detailList[i].transition = "width 0s";
+        detailList[i].transition = "height 0s, width 0s";
         detailList[i].style.width = "0";
+        detailList[i].style.height = "0";
         detailList[i].style.opacity = "0";
     }
     let content = document.getElementsByClassName("hide");
@@ -35,19 +36,22 @@ function reset() {
 for (let i = 0; i < divCount; i++) {
     infoList[i].addEventListener("click", function() {
         reset();
-        let detail = `#detail_${i + 1} .hide`;
-        let content = document.querySelectorAll(detail)
-        detailList[i].style.transition = "width 3s";
-        detailList[i].style.width = "100%";
-        detailList[i].style.opacity = "1";
         setTimeout(function() {
-            for (let x = 0; x <= content.length - 1; x++) {
-                setTimeout(function() {
-                    content[x].style.transition = "opacity 3s";
-                    content[x].style.opacity = "1";
-                }, 1000 * x);
-            }
-        }, 2000);
+            let detail = `#detail_${i + 1} .hide`;
+            let content = document.querySelectorAll(detail)
+            detailList[i].style.transition = "height 3s, width 3s";
+            detailList[i].style.width = "100%";
+            detailList[i].style.height = "550px";
+            detailList[i].style.opacity = "1";
+            setTimeout(function() {
+                for (let x = 0; x <= content.length - 1; x++) {
+                    setTimeout(function() {
+                        content[x].style.transition = "opacity 3s";
+                        content[x].style.opacity = "1";
+                    }, 1000 * x);
+                }
+            }, 2000);
+        }, 500);
     });
 }
     
